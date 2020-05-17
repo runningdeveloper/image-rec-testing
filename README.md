62e2aa3f81e9423d42cef5fe764a51f31f3661a1
Geoffrey <info@runningdeveloper.com>
2020-05-17 17:26:51 +0200

    basic start

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # image-rec-testing
+
+## Hypothesis 
+
+Doing a tensoflow prediction on an image is a heavy task. Maybe I don't need to send every image to get a prediction. For a security camera or bird watching thing. Could I save battery, resources and etc without having to do predictions continously.
+The idea is running this on a raspberry pi.
+
+## TODO
+
+- [x] Make a simple image recognition script
+- [ ] Do a performance test doing image recognition 
+- [ ] See if I can get an image diffing thing to work
+- [ ] Do a performace test on the diffing function
+- [ ] Test the difference and see if this is an effective method
+
+## How (So I remeber what I did)
+
+- Using the inspect function in node (https://nodejs.org/en/docs/guides/debugging-getting-started/)[https://nodejs.org/en/docs/guides/debugging-getting-started/]
+- When I run ```test:tensorflow``` I can then open up chrome and ```chrome://inspect/#devices``` to get the inspect view.
+- Then look at the profiler and memory stuff.
+- Having trouble running the profiler when the script runs and then quits
---
+++ tensorflow-prediction.js
@@ -0,0 +1,40 @@
+// Simple prediction
+
+const tf = require("@tensorflow/tfjs-node");
+const cocoSsd = require("@tensorflow-models/coco-ssd");
+const fs = require("fs");
+const readline = require("readline");
+
+let model = null;
+
+const doImagePrediction = async () => {
+  const imageFile = fs.readFileSync("./images/person1.jpg");
+  console.time('Prediction');
+  const img = tf.node.decodeImage(imageFile);
+  const predictions = await model.detect(img);
+  console.timeEnd('Prediction');
+  console.log({ predictions });
+};
+
+(async function() {
+  //   readline.emitKeypressEvents(process.stdin);
+  //   process.stdin.setRawMode(true);
+
+  console.log("Loading model");
+  model = await cocoSsd.load();
+  //   console.log("Press g when ready or q to quit");
+
+  setTimeout(()=>doImagePrediction(), 5000)
+
+  
+
+  //   process.stdin.on("keypress", (key, data) => {
+  //     if (data.name === "q") {
+  //       process.exit();
+  //     } else if (data.name === "g") {
+  //       doImagePrediction();
+  //     } else {
+  //       console.log("Unrecognised key press");
+  //     }
+  //   });
+})();
